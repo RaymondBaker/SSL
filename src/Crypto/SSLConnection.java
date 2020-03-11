18be15d4a897ed2a323a5553032bbf9f81f1874a
Raymond Baker <raymond_soap@live.com>
2020-03-11 12:49:36 -0400

    finished stub out

--- src/Crypto/SSLConnection.java
+++ src/Crypto/SSLConnection.java
@@ -36,7 +36,17 @@ public class SSLConnection {
         // recv cert_request
         // recv server_hello_done
         
-        //
+        // ----Phase 3----
+        // send cert
+        // send client_key_exchange
+        // send cert_verify
+        
+        // ----Phase 4----
+        // send change_cipher_spec
+        // send finished
+        // recv change_cipher_spec
+        // recv finished
+        
         handshake_complete = true;
 
     }
@@ -50,6 +60,18 @@ public class SSLConnection {
         // send cert
         // send cert_request
         // send server_hello_done
+        
+        
+        // ----Phase 3----
+        // recv cert
+        // recv client_key_exchange
+        // recv cert_verify
+        
+        // ----Phase 4----
+        // recv change_cipher_spec
+        // recv finished
+        // send change_cipher_spec
+        // send finished
         
         handshake_complete = true;
     }
